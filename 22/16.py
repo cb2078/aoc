43ed14a0e3c21272391b0aaa077497598f3eeeed
unknown <cb2078@york.ac.uk>
2023-10-13 00:14:13 +0100

    optimize 2022 day 16

--- 22/16.py
+++ 22/16.py
@@ -1,29 +1,22 @@
-f=[x.split() for x in open('16a.txt').read().strip().split('\n')]
+f=[x.split() for x in open('16.txt').read().strip().split('\n')]
 v=[x[1] for x in f];aa=v.index('AA')
 n=len(v)
 g=[[v.index(n.replace(',','')) for n in x[9:]] for x in f]
 r=[int(''.join(filter(str.isdigit,x[4]))) for x in f]
 
-d=[[0 if i==j else 1 if j in g[i] else float('inf')
-    for j in range(n)] for i in range(n)]
+d=[[1 if j in g[i] else float('inf') for j in range(n)] for i in range(n)]
 from itertools import product
 for k,i,j in product(range(n),repeat=3):
     d[i][j]=min(d[i][j],d[i][k]+d[k][j])
 
-def dfs(u,p,t,o):
-    y = 0
-    if o > 0:
-        y = max(y, dfs(aa, p | 1<<u, 26, o - 1))
-    if t <= 0:
-        return 0
-    x = r[u] * t
+def dfs(u,t,p=0,x=0):
+    p |= 1 << u
+    dp[p] = max(dp.get(p, 0), x)
     for c in range(n):
-        if 1<<c & p or c == u:
+        T = t - 1 - d[u][c]
+        if 1 << c & p or r[c] == 0 or T <= 0:
             continue
-        if not r[c]:
-            continue
-        y = max(y, dfs(c, p | 1<<u, t - 1 - d[u][c], o))
-    return x + y
+        dfs(c, T, p, x + r[c] * T)
 
-print(dfs(aa, 0, 30, 0)) # 1
-print(dfs(aa, 0, 26, 1)) # 2
+dp = {}; dfs(aa, 30); print(max(dp.values()))
+dp = {}; dfs(aa, 26); print(max(dp[x] + dp[y] for x in dp for y in dp if x & y == 1 << aa))
